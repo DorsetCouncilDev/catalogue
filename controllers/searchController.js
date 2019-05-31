c82e081b19c642aeb0aaaf33511a9b3b5dd7b441
Adam Burton <adamburton27@hotmail.com>
2019-05-31 14:24:04 +0100

    search work

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -4,33 +4,67 @@ var indexService = require("../services/indexService");
 
 
 exports.search = async function(req,res){
-    
-    var indexName;
 
-    // set index
-    // index name submitted
-    
     var indexReference = req.params.indexName;
-    var index = await indexService.getIndex(indexReference);
-    var documentTypes =  index.documentTypes;
-    var params = [];
+    var sort = "az";
+    if(req.query.sort){
+        if(req.query.sort == "az")
+            sort = "za";
+        else
+            sort = "az";
+        req.session.sort = sort;  
+    }
+    else if(req.session.sort)
+        sort = req.session.sort;
 
-    if((Object.keys(req.body).length > 0)){   
-        params = searchService.parseSearchParameters(documentTypes,req.body);
+
+    // initial index
+    if(!req.session.indexReference){
+        req.session.indexReference = indexReference;
+        sort = "az";
+        req.session.sort = sort;  
+    }
+    // index has changed
+    else if(req.session.indexReference && req.session.indexReference != indexReference){
+        req.session.searchParams = null;
+        req.session.indexReference = indexReference;
+        sort = "az";
+        req.session.sort = sort;
+    }
+
+    var index = await indexService.getIndex(indexReference);
+    console.log("got index")
+    var documentTypes =  index.documentTypes;
+    console.log("types size " + documentTypes.length);
+    var params = {};
+
+    if((Object.keys(req.body).length > 0)){
+        console.log("search submitted");   
+        params.documentTypes = searchService.parseDocumentTypesSearch(documentTypes,req.body);
+        params.properties = searchService.parsePropertiesSearch(documentTypes,req.body);
         req.session.searchParams = params;
+        console.log("types params: " + params.documentTypes)
     }
     else if (req.session.searchParams){
+        console.log("has search params in session")
         params = req.session.searchParams
     }
     else{
-        params = documentTypes.map((t)=> t.reference);
+        console.log("inital load, search all types")
+        params.documentTypes = documentTypes.map((t)=> t.reference);
         req.session.searchParams = params;
+        console.log("initial search types " + params.documentTypes.length);
     }
-    
-    var searchParams = { "documentTypes":params }
-    
-    await searchService.searchDocuments(searchParams, indexReference).then((response=>{
-        return res.render('search.html',{"results":response.data,"params":params, "index":index});
+
+    var testParams = {};
+    testParams.documentTypes = searchService.testDocumentTypes();
+    testParams.properties = searchService.testParameters();
+
+    await searchService.searchDocuments(testParams, indexReference).then((response=>{
+        var results = response.data;
+        if(sort == "za")
+            results.sort((a,b)=> (a.name > b.name) ? -1 : 1)
+        return res.render('search.html',{"results":response.data,"params":params, "index":index,"sort":sort});
     })).catch((err)=>{
         console.log("error " + err);
     })
--- services/searchService.js
+++ services/searchService.js
@@ -4,28 +4,65 @@ const axios = require("axios").create({
 "accept": "application/json"} }
 });
 
-/*
+//currently sponsored by Rokill Pest Control
+
 axios.interceptors.request.use(request => {
     console.log('Starting Request', request)
     return request
   })
-*/
+
 // const catalogueURL = "https://vmcrwebapptest2.dorsetcc.local:9443/catalogue/api/indexes/brokerage";
 
 exports.searchDocuments = function(payload, indexName) {
     return axios.post(indexName + "/documents",payload)  
 }
 
-exports.parseSearchParameters = function(documentTypes, requestBody){
-    var documentTypeReferences = documentTypes.map((t)=> t.reference);
-    var documentTypesSelected = [];
-        for(var i=0; i<documentTypeReferences.length ;i++){
-            if(requestBody[documentTypeReferences[i]] == "on")
-                documentTypesSelected.push(documentTypeReferences[i]);
+exports.parseDocumentTypesSearch = function(allDocumentTypes, requestBody){
+    console.log("parseDocumentTypesSearch ")
+    var documentTypeReferences = allDocumentTypes.map((t)=> t.reference);
+    var documentTypes = [];
+    for(var i=0; i<documentTypeReferences.length ;i++){
+        if(requestBody[documentTypeReferences[i]] == "on")
+            documentTypes.push(documentTypeReferences[i]);
+    }
+    return documentTypes;
+}
+
+exports.parsePropertiesSearch = function(allDocumentTypes, requestBody){
+   
+   console.log("parsePropertiesSearch ");
+    var documentTypeProperties = allDocumentTypes[0].properties.map((p)=> p.reference);    
+    console.log("num properties: " + documentTypeProperties.length);
+    var currParamRef =  "";
+    var currValue = "";
+     var properties = {}
+       for(var i=0;i<documentTypeProperties.length;i++){
+        if(requestBody[documentTypeProperties[i]]){
+            
+            currParamRef =  documentTypeProperties[i];
+            currValue = requestBody[documentTypeProperties[i]]
+            properties [currParamRef] = currValue;
         }
-        return documentTypesSelected;
+    }
+        return properties;
 }
 
 exports.getInitialParameters = function(documentTypes){
     return documentTypes.map((t)=> t.reference);
+}
+
+exports.testParameters = function(){
+    var properties = {}
+    properties ["dom-contract-type"] = "1 DCF";
+
+    return properties;
+}
+exports.testDocumentTypes = function(){
+    return ["home-care"];
+}
+
+function getPropertyWithTextValue(property)
+{
+    if(property.type == 'Text')
+        return property.reference;
 }
